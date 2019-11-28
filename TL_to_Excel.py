f26493af615ac1f867589045796b68ef3217d7a0
Talon Jones <taloncjones@gmail.com>
2019-11-27 16:22:31 -0800

    Add basic data -> xls write

--- TL_to_Excel.py
+++ TL_to_Excel.py
@@ -1,9 +1,21 @@
 import xml.etree.ElementTree as ET
+import xlwt
+
+def write_row(sheet, row, data):
+    r = sheet.row(row)
+    for index, col in enumerate(data):
+        r.write(index, col)
+    return row + 1
 
 tree = ET.parse('testsuites_all.xml')
 root = tree.getroot()
 
-impact_level = ['Smoke','Low','Med','High']
+impact_level = ['Smoke', 'Low', 'Med', 'High']
+
+book = xlwt.Workbook()
+sheet1 = book.add_sheet('PySheet1')
+cols = ['Feature', 'Impact Level', 'Test Case']
+row = write_row(sheet1, 0, cols)
 
 print(root.get('name'))
 
@@ -16,7 +28,11 @@ for feature in root.findall('./*[@name]'):
         for testcase in feature.findall(f".//*[@name='AAs']/*[@name='{impact}']/testcase"):
             test_name = testcase.get('name')
             print(test_name)
+            row = write_row(sheet1, row, [feature_name, impact, test_name])
         # Same as above but checks for AA folder (numerous features seem to have this typo)
         for testcase in feature.findall(f".//*[@name='AA']/*[@name='{impact}']/testcase"):
             test_name = testcase.get('name')
             print(test_name)
+            row = write_row(sheet1, row, [feature_name, impact, test_name])
+
+book.save('test.xls')
